b6d50c17de942758c50b01dafce0aa6c46b0d1f7
John-Sanchez-Chilo <82964968+John-Sanchez-Chilo@users.noreply.github.com>
2022-08-19 23:16:58 -0500

    Update CoordinadorControlador.py

--- blogex/controller/CoordinadorControlador.py
+++ blogex/controller/CoordinadorControlador.py
@@ -1,31 +1,40 @@
-from flask import Flask, render_template,session,json
+from flask import Flask, render_template, request, json, redirect,session
 from flaskext.mysql import MySQL
+
 
 app = Flask(__name__)
 mysql = MySQL()
+app.secret_key = 'secreto'
+#MySQL Configuracion
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'admin_1572003_jesc'
+app.config['MYSQL_DATABASE_DB'] = 'tienda_lentes'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
 
-class ControladorCoordinador():
-    @app.route("/coordinadorHome")
-    def main():
-        return render_template('indexCoord.html')
 
-    @app.route('/getSolicitud')
-    def getSolicitud():
-        if session.get('user'):
-            _user = session.get('user')
-            conn = mysql.connect()
-            cursor = conn.cursor()
-            cursor.execute('SELECT id_solicitud,nombre,apellidoPaterno, apellidoMaterno, correo FROM Solicitudes;')
-            data = cursor.fetchall()
-            solicitud_list = []
-            for solicitud in data:
-                solicitud_list = {
-                    'Id_solicitud': str(solicitud[0]),
-                    'Nombre': solicitud[1],
-                    'apellidoPaterno':solicitud[2],
-                    'apellidoMaterno':solicitud[3],
-                    'correo':solicitud[4]}
-                solicitud_list.append(solicitud_list)
-            return json.dumps(solicitud_list)
-        else:
-            return render_template('error.html', error='Acceso no Autorizado')
+
+@app.route("/")
+def main():
+    return render_template('indexCoord.html')
+
+@app.route('/get_solicitud')
+def get_solicitud():
+    if session.get('user'):
+        _user = session.get('user')
+        conn = mysql.connect()
+        cursor = conn.cursor()
+        cursor.execute('SELECT id_solicitud,nombre,apellidoPaterno, apellidoMaterno, correo FROM Solicitudes;')
+        data = cursor.fetchall()
+        solicitud_list = []
+        for solicitud in data:
+            solicitud_list = {
+                'Id_solicitud': str(solicitud[0]),
+                'Nombre': solicitud[1],
+                'apellidoPaterno':solicitud[2],
+                'apellidoMaterno':solicitud[3],
+                'correo':solicitud[4]}
+            solicitud_list.append(solicitud_list)
+        return json.dumps(solicitud_list)
+    else:
+        return render_template('error.html', error='Acceso no Autorizado')
