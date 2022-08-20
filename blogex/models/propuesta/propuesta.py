33c98033c864af2fdc9891479f6482ea03f63963
John-Sanchez-Chilo <82964968+John-Sanchez-Chilo@users.noreply.github.com>
2022-08-19 23:16:28 -0500

    Update propuesta.py

--- blogex/models/propuesta/propuesta.py
+++ blogex/models/propuesta/propuesta.py
@@ -1,1 +1,12 @@
+from models.Evento import Evento
+class Propuesta(object):
+    def __init__(self,id_propuesta=None, titulo=None, descripcion=None, tema=None,):
+        self.id_propuesta = id_propuesta
+        self.titulo = titulo
+        self.descripcion = descripcion
+        self.tema = tema
 
+    #funcion para pasar una propuesta aceptada a un evento ascociando al encargado
+    def convertir_a_evento(self,encargado):
+        evento=Evento(self.id_propuesta,self.titulo,self.descripcion,encargado,self.tema)
+        return evento
