37e997219c45f573464670349cac8abffb5bacdd
John-Sanchez-Chilo <82964968+John-Sanchez-Chilo@users.noreply.github.com>
2022-08-22 13:58:17 -0500

    Update usuarioNormal.py

--- blogex/models/usuario/usuarioNormal.py
+++ blogex/models/usuario/usuarioNormal.py
@@ -1,1 +1,6 @@
 
+from usuario import Usuario
+class Usuario_Normal(Usuario):
+    def __init__(self,id_usuario,nombre,apellido_paterno,apellido_materno,correo,telefono,centro_estudios,usuario,contraseña):
+        super().__init__(id_usuario,nombre,apellido_paterno,apellido_materno,correo,telefono,usuario,contraseña)
+        self.centro_estudios=centro_estudios
