dee11098d638f041fe47b1ced25cbb6f29baf3db
John-Sanchez-Chilo <82964968+John-Sanchez-Chilo@users.noreply.github.com>
2022-08-22 13:58:04 -0500

    Update usuario.py

--- blogex/models/usuario/usuario.py
+++ blogex/models/usuario/usuario.py
@@ -1,1 +1,12 @@
-
+from sistema_publicacion_eventos.models.utilidad.nombre import *
+class Usuario:
+    def __init__(self,id_usuario,nombre,apellido_paterno,apellido_materno,correo,telefono,usuario,contraseña):
+        self.id_usuario=id_usuario
+        self.nombre_completo=Nombre(nombre,apellido_paterno,apellido_materno)
+        self.correo=correo
+        self.telefono=telefono
+        self.usuario=usuario
+        self.contraseña=contraseña
+        
+    def get_nombre(self):
+        return self.nombre_completo.get_nombre_completo()
