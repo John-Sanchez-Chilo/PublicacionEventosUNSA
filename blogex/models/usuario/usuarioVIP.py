6b7bc9ec9983adbe54ba8cd2389497b10ce1dc3a
John-Sanchez-Chilo <82964968+John-Sanchez-Chilo@users.noreply.github.com>
2022-08-22 13:59:00 -0500

    Update usuarioVIP.py

--- blogex/models/usuario/usuarioVIP.py
+++ blogex/models/usuario/usuarioVIP.py
@@ -1,1 +1,7 @@
 
+from usuario import Usuario
+
+class Usuario_VIP(Usuario):
+    def __init__(self,id_usuario,nombre,apellido_paterno,apellido_materno,correo,telefono,especialidad,usuario,contraseña):
+        super().__init__(id_usuario,nombre,apellido_paterno,apellido_materno,correo,telefono,usuario,contraseña)
+        self.especialidad=especialidad
