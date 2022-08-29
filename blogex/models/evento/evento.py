d100adc930dc68dd0e0ed0bd7b566bf1cb37ed7e
John-Sanchez-Chilo <82964968+John-Sanchez-Chilo@users.noreply.github.com>
2022-08-29 13:53:44 -0500

    Update evento.py

--- blogex/models/evento/evento.py
+++ blogex/models/evento/evento.py
@@ -1,2 +1,8 @@
+from models.evento import Evento
 class Evento():
-  
+    def __init__(self,id_evento,titulo,tema,descripcion, id_encargado):
+        self.id_evento = id_evento
+        self.titulo = titulo
+        self.tema = tema
+        self.descripcion = descripcion
+        self.id_encargado = id_encargado
