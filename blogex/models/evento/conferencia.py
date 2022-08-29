a8617092f3b6ed6e785b798cd86eb6db85dc22f7
John-Sanchez-Chilo <82964968+John-Sanchez-Chilo@users.noreply.github.com>
2022-08-29 13:54:38 -0500

    Update conferencia.py

--- blogex/models/evento/conferencia.py
+++ blogex/models/evento/conferencia.py
@@ -1,2 +1,7 @@
+from models.evento import Evento
 class Conferencia(Evento):
-  
+    def __init__(self,id_evento,titulo,tema,descripcion, id_encargado,id_evento_conferencia,fecha, hora):
+        super().__init__(id_evento,titulo,tema,descripcion, id_encargado)
+        self.id_evento_conferencia = id_evento_conferencia
+        self.fecha = fecha
+        self.hora = hora
