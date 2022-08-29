f46d7b2697753242f10726419ab98fae14acb225
John-Sanchez-Chilo <82964968+John-Sanchez-Chilo@users.noreply.github.com>
2022-08-29 13:54:10 -0500

    Update workshop.py

--- blogex/models/evento/workshop.py
+++ blogex/models/evento/workshop.py
@@ -1,2 +1,7 @@
+from models.evento import Evento
 class Workshop(Evento):
-  
+    def __init__(self,id_evento,titulo,tema,descripcion, id_encargado,id_evento_conferencia,fecha_inicio, fecha_fin):
+        super().__init__(id_evento,titulo,tema,descripcion, id_encargado)
+        self.id_evento_conferencia = id_evento_conferencia
+        self.fecha_inicio = fecha_inicio
+        self.fecha_fin = fecha_fin
