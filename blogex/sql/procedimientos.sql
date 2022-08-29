063232be0d6e211071302fdcc8f5792c0610b944
Diego Rivas <drivash@unsa.edu.pe>
2022-08-29 12:11:14 -0500

    Creacion de usuario

--- blogex/sql/procedimientos.sql
+++ blogex/sql/procedimientos.sql
@@ -49,3 +49,34 @@ SELECT* FROM Usuario;
 SELECT* FROM UsuarioNormal;
 SELECT* FROM UsuarioVip;
 CALL validarLogin('usuarioDos');
+
+#Creacion de usuarios
+USE UNSA;
+#DROP PROCEDURE crearUsuario
+DELIMITER $$
+create definer=`root`@`localhost` procedure crearUsuario(
+IN n_usuario varchar(30),
+IN n_contrasena varchar(30),
+IN n_nombre varchar(30),
+IN n_apellido_paterno varchar(30),
+IN n_apellido_materno varchar(30),
+IN n_correo varchar(30),
+IN n_telefono varchar(30),
+IN n_estudios varchar(30),
+IN n_descripcion varchar(30))
+begin
+	if (select exists (select 1 from Usuario where usuario = n_usuario)) then
+		select 'Usuario ya existe!!';
+	else
+        insert into Usuario 
+		values (null,n_usuario, n_contrasena, n_nombre, n_apellido_paterno, n_apellido_materno, n_correo, n_telefono, n_estudios,n_descripcion);
+		
+    end if;
+end$$
+DELIMITER ;
+
+USE UNSA;
+SELECT* FROM usuario;
+
+USE UNSA;
+CALL crearUsuario('usuarioTres','222','Juan','Perez','Rios','a@gmail.com','111111111','Matematica','jaansn');
