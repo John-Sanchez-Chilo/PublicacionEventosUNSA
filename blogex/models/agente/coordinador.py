0305e0b31765a3e8d46d10fe526ccd11abd59da6
John-Sanchez-Chilo <82964968+John-Sanchez-Chilo@users.noreply.github.com>
2022-08-29 13:53:29 -0500

    Update coordinador.py

--- blogex/models/agente/coordinador.py
+++ blogex/models/agente/coordinador.py
@@ -1,1 +1,6 @@
 
+class Coordinador:
+    def __init__(self,id_coordinador,nombre, especialidad):
+        self.id_coordinador = id_coordinador
+        self.nombre = nombre
+        self.especialidad = especialidad
